16032b10e2a7f2be5b05dc2fcc2df9522fb33a20
meruz <manuel.baltieri@gmail.com>
2016-05-31 23:33:06 +0100

    Added motors as proprioceptors (?), they are now "senses".
    Active inference is in, at the moment all it does is to try to fulfil a prediction, need to introduce priors.

--- brait_veh_PP.py
+++ brait_veh_PP.py
@@ -23,10 +23,12 @@ theta = 0
 
 # sensors
 sensors_n = 2
-sensor = np.zeros((2,))
+motors_n = 2
+variables = sensors_n + motors_n
+sensor = np.zeros((sensors_n,))
 
 # network (brain)
-temp_orders = 2
+temp_orders = 1
 nodes = 3
 
 x = np.zeros((iterations,nodes,temp_orders))
@@ -36,27 +38,33 @@ n = .0*np.random.standard_normal((iterations,nodes))
 n = np.zeros((iterations,nodes))
 w_orig = np.random.standard_normal((nodes,nodes))
 
-# inference
-mu_x = np.zeros((sensors_n,temp_orders))
+# perceptual inference
+rho = np.zeros((variables,temp_orders))
 
-eps_z = np.zeros((sensors_n,temp_orders))
-eps_w = np.zeros((sensors_n,temp_orders))
-xi_z = np.zeros((sensors_n,temp_orders))
-xi_w = np.zeros((sensors_n,temp_orders))
-pi_z = 100*np.ones((sensors_n,temp_orders))
-pi_w = 1000*np.ones((sensors_n,temp_orders))
+mu_x = np.zeros((variables,temp_orders))
+
+eps_z = np.zeros((variables,temp_orders))
+eps_w = np.zeros((motors_n,temp_orders))
+xi_z = np.zeros((variables,temp_orders))
+xi_w = np.zeros((motors_n,temp_orders))
+pi_z = 100*np.ones((variables,temp_orders))
+#pi_z[2:4,0] *= .000000001
+pi_w = 1000*np.ones((motors_n,temp_orders))
 sigma_z = 1/(np.sqrt(2*pi_z))
 sigma_w = 1/(np.sqrt(2*pi_w))
 
 FE = np.zeros((iterations,))
 
+# active inference
+a = np.zeros((motors_n,temp_orders))
+
 # noise
-z = np.zeros((sensors_n,iterations))
+z = np.zeros((variables,iterations))
 z[0,:] = sigma_z[0,0]*np.random.randn(1,iterations)
 z[1,:] = sigma_z[1,0]*np.random.randn(1,iterations)
 
 # fluctuations
-w = np.zeros((sensors_n,iterations))
+w = np.zeros((motors_n,iterations))
 w[0,:] = sigma_w[0,0]*np.random.randn(1,iterations)
 w[1,:] = sigma_w[1,0]*np.random.randn(1,iterations)
 
@@ -67,10 +75,11 @@ vel_centre_history = np.zeros((1,iterations))
 vel_history = np.zeros((2,iterations))
 theta_history = np.zeros((1,iterations))
 orientation_history = np.zeros((2,2,iterations))
-sensor_history = np.zeros((2,iterations))
-rho_history = np.zeros((2,iterations))                  # noisy version of the sensors
+sensor_history = np.zeros((sensors_n,iterations))
+rho_history = np.zeros((variables,iterations))                  # noisy version of the sensors
 
-mu_x_history = np.zeros((iterations,sensors_n,temp_orders))
+mu_x_history = np.zeros((iterations,variables,temp_orders))
+a_history = np.zeros((iterations,motors_n))
 
 ### environment ###
 
@@ -127,14 +136,14 @@ x[0,:,0] = x_init
 w_orig = np.array([[ 1.12538509, -2.00524372, 0.64383674], [-0.61054784, 0.15221595, -0.36371622], [-0.02720039, 1.39925152, 0.84412855]])
 alpha = 1*np.ones((nodes,))
 
-eta_mu_x = .01*np.ones((sensors_n,temp_orders))
-
-a = np.zeros((2,iterations))
+eta_mu_x = .01*np.ones((variables,temp_orders))
+eta_a = .01*np.ones((motors_n,1))
 
 for i in range(iterations-1):
+    print(i)
     # brain
-    x[i,:,1] = 1/alpha*(- x[i,:,0] + np.tanh(np.dot(w_orig,x[i,:,0]))) + n[i,]
-    x[i+1,:,0] = x[i,:,0] + dt*x[i,:,1]    
+#    x[i,:,1] = 1/alpha*(- x[i,:,0] + np.tanh(np.dot(w_orig,x[i,:,0]))) + n[i,]
+#    x[i+1,:,0] = x[i,:,0] + dt*x[i,:,1]
     
     # perception
     sensor[0] = light_level(pos_centre + radius*(np.array([[np.cos(theta+sensors_angle)], [np.sin(theta+sensors_angle)]])))            # left sensor
@@ -145,8 +154,8 @@ for i in range(iterations-1):
 #    vel[1] = x[i,1,1]                   # attach neuron to motor
     
     # vehicle 2
-    vel[0] = f(sensor[1])                   # attach neuron to motor
-    vel[1] = f(sensor[0])                   # attach neuron to motor
+    vel[0] = f(sensor[1]) + np.tanh(a[0])                   # attach neuron to motor
+    vel[1] = f(sensor[0]) + np.tanh(a[1])                   # attach neuron to motor
     
     # vehicle 3
 #    vel[0] = f(sensor[0])                   # attach neuron to motor
@@ -163,34 +172,37 @@ for i in range(iterations-1):
     ### inference ###
     
     # add noise and fluctuations
-    rho = sensor + z[:,i]
-    mu_x[:,1] += w[:,i]
+    rho[0:sensors_n,0] = sensor + z[0:sensors_n,i]
+    rho[sensors_n:variables,0] = np.squeeze(vel) + z[sensors_n:variables,i]
     
+    mu_x[sensors_n:variables,0] += w[:,i]
 
-    eps_z[:,0] = rho - mu_x[:,0]
+    eps_z[:,0] = np.squeeze(rho - mu_x)
     xi_z[:,0] = pi_z[:,0]*eps_z[:,0]
     
-    eps_w[:,0] = mu_x[:,1] - f(mu_x[:,0])
+    eps_w[:,0] = mu_x[sensors_n:variables,0] - f(mu_x[0:sensors_n,0])
     xi_w[:,0] = pi_w[:,0]*eps_w[:,0]
     
     FE[i] = .5*(np.trace(np.dot(eps_z,np.transpose(xi_z))) + np.trace(np.dot(eps_w,np.transpose(xi_w))))
     
     # perception
-    a[:,i] = xi_z[:,0]*-1 + xi_w[:,0]*dfdmu_x(mu_x[:,0])
-    b = xi_w[:,0]
-    dFdmu_x = np.transpose(np.array([xi_z[:,0]*-1 + xi_w[:,0]*-dfdmu_x(mu_x[:,0]), xi_w[:,0]]))
-    
+    #dFdmu_x = np.transpose(np.array([xi_z[:,0]*-1 + xi_w[:,0]*-dfdmu_x(mu_x[:,0]), xi_w[:,0]]))
+    dFdmu_x = np.transpose(np.array([xi_z[:,0]*-1 + np.concatenate([xi_w[:,0]*-dfdmu_x(mu_x[0:sensors_n,0]), xi_w[:,0]])]))    
     mu_x += dt* -eta_mu_x*dFdmu_x
     
+    # action
+    dFda = np.transpose(np.array([xi_z[sensors_n:variables,0]*(1-np.tanh(a[:,0])**2)]))
+    a += dt* -eta_a*dFda
+    
     # update plot
-    if np.mod(i,200)==0:                                                                    # don't update at each time step, too computationally expensive
-        orientation_endpoint = pos_centre + length_dir*(np.array([[np.cos(theta)], [np.sin(theta)]]))
-        orientation = np.concatenate((pos_centre,orientation_endpoint), axis=1)
-        line1.set_xdata(pos_centre[0])
-        line1.set_ydata(pos_centre[1])
-        line2.set_xdata(orientation[0,:])
-        line2.set_ydata(orientation[1,:])
-        fig.canvas.draw()
+#    if np.mod(i,200)==0:                                                                    # don't update at each time step, too computationally expensive
+#        orientation_endpoint = pos_centre + length_dir*(np.array([[np.cos(theta)], [np.sin(theta)]]))
+#        orientation = np.concatenate((pos_centre,orientation_endpoint), axis=1)
+#        line1.set_xdata(pos_centre[0])
+#        line1.set_ydata(pos_centre[1])
+#        line2.set_xdata(orientation[0,:])
+#        line2.set_ydata(orientation[1,:])
+#        fig.canvas.draw()
     #input("\nPress Enter to continue.")                                                    # adds a pause
 
     # save data
@@ -199,9 +211,10 @@ for i in range(iterations-1):
     vel_history[:,i] = vel[:,0]
     theta_history[:,i] = theta    
     sensor_history[:,i] = sensor[:]
-    rho_history[:,i] = rho[:]
+    rho_history[:,i] = np.squeeze(rho[:])
     
     mu_x_history[i,:,:] = mu_x
+    a_history[i,:] = a[:,0]
     
 plt.figure(1)
 plt.plot(pos_centre_history[0,:-1], pos_centre_history[1,:-1])
@@ -217,10 +230,18 @@ plt.title("Inferred light level")
 
 plt.figure(3)
 plt.subplot(1,2,1)
-plt.plot(range(iterations), vel_history[0,:], 'b', range(iterations), mu_x_history[:,0,1], 'r')
+plt.plot(range(iterations), vel_history[0,:], 'b', range(iterations), mu_x_history[:,2,0], 'r')
 plt.subplot(1,2,2)
-plt.plot(range(iterations), vel_history[1,:], 'b', range(iterations), mu_x_history[:,1,1], 'r')
+plt.plot(range(iterations), vel_history[1,:], 'b', range(iterations), mu_x_history[:,3,0], 'r')
 plt.title("Inferred speed")
+
+plt.figure(4)
+plt.subplot(1,2,1)
+plt.plot(range(iterations), a_history[:,0])
+plt.subplot(1,2,2)
+plt.plot(range(iterations), a_history[:,1])
+
+
 
 
 
