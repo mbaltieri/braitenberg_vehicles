03e35ae444c5f837d2d740cb4e3ac6d8640154bd
Manuel <m.baltieri@sussex.ac.uk>
2017-03-07 15:02:09 +0000

    There looks to be a difference with Qt5 in the way you update a plot online.

--- rwat.py
+++ rwat.py
@@ -8,16 +8,18 @@ Created on Thu Apr 28 17:11:02 2016
 import matplotlib.pyplot as plt
 import numpy as np
 
+plt.close('all')
+
 x = np.linspace(0, 6*np.pi, 100)
 y = np.sin(x)
 
-# You probably won't need this if you're embedding things in a tkinter plot...
+plt.axis([0, 10, 0, 1])
 plt.ion()
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-line1, = ax.plot(x, y, 'r-') # Returns a tuple of line objects, thus the comma
+for i in range(10):
+    y = np.random.random()
+    plt.scatter(i, y)
+    plt.pause(0.05)
 
-for phase in np.linspace(0, 10*np.pi, 500):
-    line1.set_ydata(np.sin(x + phase))
-    fig.canvas.draw()
+#while True:
+#    plt.pause(0.05)
