743af939448f82a67cc287fd180c0fdbbb899b8b
Manuel <m.baltieri@sussex.ac.uk>
2017-03-08 18:32:20 +0000

    Found mistake in the sigmoid function the processed motor action, corrected it.
    Still trying to recreate a traditional Braitenberg vehicle for comparison.

--- Braitenberg_revisited.py
+++ Braitenberg_revisited.py
@@ -67,7 +67,7 @@ z = (np.dot(np.diag(sigma_z), np.random.randn(obs_states, iterations))).transpos
 
 # noise on motion of hidden states
 gamma_w = 12 * np.ones((hidden_states, ))    # log-precision
-pi_w = 0 * np.ones((hidden_states, ))
+pi_w = np.exp(gamma_w) * np.ones((hidden_states, ))
 sigma_w = 1 / (np.sqrt(pi_w))
 w = (np.dot(np.diag(sigma_w), np.random.randn(obs_states, iterations))).transpose()
 
@@ -100,16 +100,16 @@ def light_level(x_agent):
 def g(x, v):
     return x
 
-def f(x_agent, v, w, a, i):    
+def f(x_agent, v_agent, v_motor, theta, v, w, a, i):    
         # vehicle 2
 #    v_motor[0] =  f(a[1])
 #    v_motor[1] =  f(a[0])
     
     # vehicle 3
-    v_motor[i, 0] = max_speed * (sigmoid(1 - a[0]))
-    v_motor[i, 1] = max_speed * (sigmoid(1 - a[1]))
+    v_motor[i, 0] = max_speed * (1 - sigmoid(a[0]))
+    v_motor[i, 1] = max_speed * (1 - sigmoid(a[1]))
     
-#    v_motor[:,0] += + w
+#    print(v_motor[i, :])
     
     # translation
     v_agent[i] = (v_motor[i, 0] + v_motor[i, 1]) / 2
@@ -118,7 +118,7 @@ def f(x_agent, v, w, a, i):
 #    print(x_agent[i, :])
     
     # rotation
-    omega = 100 * np.float((v_motor[i, 1] - v_motor[i, 0]) / (2 * radius))
+    omega = 30 * np.float((v_motor[i, 1] - v_motor[i, 0]) / (2 * radius))
     theta[i + 1] = theta[i] + dt * omega
     theta[i + 1] = np.mod(theta[i + 1], 2 * np.pi)
     
@@ -137,8 +137,8 @@ def g_gm(x, v):
 def f_gm(x, v):
     return v
 
-def getObservation(x_agent, v, w, z, a, iteration):
-    x = f(x_agent, v, w, a, i)
+def getObservation(x_agent, v_agent, v_motor, theta, v, w, z, a, iteration):
+    x = f(x_agent, v_agent, v_motor, theta, v, w, a, iteration)
     return (g(x, v), g(x, v) + z)
 
 def sensoryErrors(y, mu_x, mu_v, mu_gamma_z):
@@ -174,7 +174,7 @@ def FreeEnergy(y, mu_x, mu_v, mu_gamma_z, mu_gamma_w, eta):
 ### initialisation
 v = np.array([l_max, l_max])
 mu_v[0, :] = v
-mu_x[0, :] = v
+#mu_x[0, :] = v
 
 #drhoda = np.array([[0., 1.], [1., 0.]])
 drhoda = - np.array([[1., 0.], [0., 1.]])
@@ -204,22 +204,21 @@ line2, = ax.plot(orientation[0, :], orientation[1, :], color='black', linewidth=
 
 
 for i in range(iterations - 1):
-    s[i, :], rho[i, :] = getObservation(x_agent, v, w[i, :], z[i, :], a[i, :], i)
+    s[i, :], rho[i, :] = getObservation(x_agent, v_agent, v_motor, theta, v, w[i, :], z[i, :], a[i, :], i)
     
     # update plot
 #    if np.mod(i, 1)==0:                                                                    # don't update at each time step, too computationally expensive
-#    orientation_endpoint = x_agent[i, :, None] + length_dir * (np.array([[np.cos(theta[i])], [np.sin(theta[i])]]))
-#    orientation = np.concatenate((x_agent[i, :, None], orientation_endpoint), axis=1)
-#    line1.set_xdata(x_agent[i, 0])
-#    line1.set_ydata(x_agent[i, 1])
-#    line2.set_xdata(orientation[0,:])
-#    line2.set_ydata(orientation[1,:])
-#    fig.canvas.draw()
-#    plt.pause(0.05)
+    orientation_endpoint = x_agent[i, :, None] + length_dir * (np.array([[np.cos(theta[i])], [np.sin(theta[i])]]))
+    orientation = np.concatenate((x_agent[i, :, None], orientation_endpoint), axis=1)
+    line1.set_xdata(x_agent[i, 0])
+    line1.set_ydata(x_agent[i, 1])
+    line2.set_xdata(orientation[0,:])
+    line2.set_ydata(orientation[1,:])
+    fig.canvas.draw()
+    plt.pause(0.05)
 #    input("\nPress Enter to continue.")
 
     eps_z, xi_z = sensoryErrors(rho[i, :], mu_x[i, :], mu_v[i, :], gamma_z)
-#    print(eps_z)
     
     FE[i] = FreeEnergy(rho[i, :], mu_x[i, :], mu_v[i, :], gamma_z, gamma_w, mu_v[i, :])
     
@@ -239,9 +238,25 @@ for i in range(iterations - 1):
     a_norm_max = 0
     a[i + 1, :] = (a_norm_max - a_norm_min) * (a[i + 1, :] - a_min) / (a_max - a_min) + a_norm_min
     
-    aa = a[i + 1, :]
-    aaa = max_speed * (sigmoid(aa))
-    
+
+#s2 = np.zeros((iterations, sensors_n))
+#theta2 = np.zeros((iterations, ))                            # orientation of the agent
+#x_agent2 = np.zeros((iterations, 2))                         # 2D world, 2 coordinates por agent position
+#v_agent2 = np.zeros((iterations, ))
+#v_motor2 = np.zeros((iterations, motors_n))
+#rho2 = np.zeros((iterations, obs_states))
+#
+#### initialisation
+#
+#x_agent2[0, :] = np.array([29., 5.])
+##x_agent2[0, :] = 100 * np.random.rand(1, 2)
+#
+##theta[0] = np.pi * np.random.rand()
+#theta2[0] = np.pi / 2 #2 / 3 * np.pi
+#
+#s2[0, :], rho2[0, :] = getObservation(x_agent2, v_agent2, v_motor2, theta2, v, w[0, :], z[0, :], rho2[0, :], 0)
+#for i in range(1, iterations - 1):
+#    s2[i, :], rho2[i, :] = getObservation(x_agent2, v_agent2, v_motor2, theta2, v, w[i, :], z[i, :], rho2[i - 1, :], i)
     
 
 plt.figure(1)
@@ -249,7 +264,7 @@ plt.plot(x_agent[:, 0], x_agent[:, 1])
 plt.xlim((0,100))
 plt.ylim((0,100))
 plt.plot(x_light[0], x_light[1], color='orange', marker='o', markersize=20)
-#plt.plot(x_agent[0, 0], x_agent[0, 1], color='red', marker='o', markersize=8)
+plt.plot(x_agent[0, 0], x_agent[0, 1], color='red', marker='o', markersize=8)
 
 plt.figure()
 plt.subplot(1, 2, 1)
@@ -307,11 +322,39 @@ cbar = light_fig.colorbar(light_map, shrink=0.5, aspect=5)
 
 
 
-
-
-
-
-
+#plt.figure()
+#plt.plot(x_agent2[:, 0], x_agent2[:, 1])
+#plt.xlim((0,100))
+#plt.ylim((0,100))
+#plt.plot(x_light[0], x_light[1], color='orange', marker='o', markersize=20)
+#plt.plot(x_agent[0, 0], x_agent[0, 1], color='red', marker='o', markersize=8)
+#
+#plt.figure()
+#plt.subplot(1, 2, 1)
+#plt.plot(x_agent2[:-1, 0], 'b', np.ones(iterations - 1) * x_light[0], 'r')
+#plt.subplot(1, 2, 2)
+#plt.plot(x_agent2[:-1, 1], 'b', np.ones(iterations - 1) * x_light[1], 'r')
+#plt.title('Position')
+#    
+#plt.figure()
+#plt.subplot(1, 2, 1)
+#plt.plot(rho2[:-1, 0], 'b', label='Noisy signal')
+#plt.plot(s2[:-1, 0], 'g', label='Signal')
+#plt.legend()
+#plt.subplot(1, 2, 2)
+#plt.plot(rho2[:-1, 1], 'b', label='Noisy signal')
+#plt.plot(s2[:-1, 1], 'g', label='Signal')
+#plt.legend()
+#
+#plt.figure()
+#plt.plot(v_motor2[:-1, 0], 'b', label='Motor1')
+#plt.plot(v_motor2[:-1, 1], 'r', label='Motor2')
+#plt.title('Velocity')
+#plt.legend()
+#
+#plt.figure()
+#plt.plot(theta2)
+#plt.title('Orientation')
 
 
 
