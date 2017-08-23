c37b2040797c86fd56f3bce0855189a152865ce6
Manuel Baltieri <M.Baltieri@sussex.ac.uk>
2017-08-23 19:55:05 +0100

    Changed the behaviour to aggressor. Checking the level of noise that would allow some comparison with some filtered sensory input (if the vehicle goes already in a straight line, no way to do better).

--- braitenberg_simple.py
+++ braitenberg_simple.py
@@ -16,7 +16,7 @@ import scipy.fftpack
 
 dt_brain = .005
 dt_world = .005
-T = 20
+T = 5
 iterations = int(T/dt_brain)
 plt.close('all')
 lag = 1
@@ -40,36 +40,34 @@ turning_speed = 30.
 
 ### Global functions ###
 
+x_light = 59
+
 def light_level(x_agent):
-    x_light = np.array([59.,47.])
     sigma_x = 30.
-    sigma_y = 30.
-    Sigma = np.array([[sigma_x ** 2, 0.], [0., sigma_y ** 2]])
-    mu = x_light
-    corr = Sigma[0, 1] / (sigma_x * sigma_y)
-    
-    return 5655 * l_max / (2 * np.pi * sigma_x * sigma_y * np.sqrt(1 - corr ** 2)) * np.exp(
-            - 1 / (2 * (1 - corr ** 2)) * ((x_agent[0] - mu[0]) ** 2 / 
-            (sigma_x ** 2) + (x_agent[1] - mu[1]) ** 2 / (sigma_y ** 2) - 
-            2 * corr * (x_agent[0] - mu[0]) * (x_agent[1] - mu[1]) / (sigma_x * sigma_y)))
+    mu = x_light    
+    return 78 * l_max / (np.sqrt(2 * np.pi) * sigma_x) * np.exp(- (x_agent[0] - mu) ** 2 / (sigma_x ** 2))
 
 
 # free energy functions
 def g(x, v):
     return x
 
-def f(x_agent, v_agent, v_motor, theta, v, w, a_det, a_stoc, i):
+def f(x_agent, v_agent, v_motor, theta, v, w, a_det, a_stoc, ff, i):
 #    # vehicle 3a - lover
-    v_motor[i, 0] = l_max - (a_det[1] + a_stoc[1])
-    v_motor[i, 1] = l_max - (a_det[0] + a_stoc[0])
+#    v_motor[i, :] = l_max - (a_det + a_stoc)
     
     # vehicle 2b - aggressor
-#    v_motor[i, 0] = a[0]
-#    v_motor[i, 1] = a[1]
+    v_motor[i, ::-1] = a_det + a_stoc
+#    v_motor[i, ::-1] = ff
     
     # translation
 #    v_agent[i] = (v_motor[i, 0] + v_motor[i, 1]) / 2
-    v_agent[i] = (l_max - (a_det[1] + a_stoc[1] / np.sqrt(dt_world)) + l_max - (a_det[0] + a_stoc[0] / np.sqrt(dt_world))) / 2
+    # vehicle 3a - lover
+#    v_agent[i] = (l_max - (a_det[1] + a_stoc[1] / np.sqrt(dt_world)) + l_max - (a_det[0] + a_stoc[0] / np.sqrt(dt_world))) / 2
+    
+    # vehicle 2b - aggressor
+    v_agent[i] = (a_det[0] + a_stoc[0] / np.sqrt(dt_world) + a_det[1] + a_stoc[1] / np.sqrt(dt_world)) / 2
+    
     x_agent[i + 1, :] = x_agent[i, :] + dt_world * (v_agent[i] * np.array([np.cos(theta[i]), np.sin(theta[i])]))
         
     # rotation
@@ -86,13 +84,11 @@ def f(x_agent, v_agent, v_motor, theta, v, w, a_det, a_stoc, i):
     
     return sensor, v_motor[i, :]
 
-def getObservation(x_agent, v_agent, v_motor, theta, v, w, z, a_det, a_stoc, iteration):
-    x, v_motor = f(x_agent, v_agent, v_motor, theta, v, w, a_det, a_stoc, iteration)
+def getObservation(x_agent, v_agent, v_motor, theta, v, w, z, a_det, a_stoc, ff, iteration):
+    x, v_motor = f(x_agent, v_agent, v_motor, theta, v, w, a_det, a_stoc, ff, iteration)
     return (g(x, v), g(x, v) + z)
 
-def Braitenberg(noise_level, desired_confidence, z2):
-    x_light = np.array([59.,47.])
-    
+def Braitenberg(noise_level, desired_confidence, z2):    
     # noise on sensory input
     gamma_z = noise_level * np.ones((obs_states, ))    # log-precisions
     pi_z = np.exp(gamma_z) * np.ones((obs_states, ))
@@ -140,15 +136,15 @@ def Braitenberg(noise_level, desired_confidence, z2):
 #    line1, = ax.plot(x_agent2[0, 0], x_agent2[0, 1], color='lightblue', marker='.', markersize=30*radius)       # Returns a tuple of line objects, thus the comma
 #    line2, = ax.plot(orientation[0, :], orientation[1, :], color='black', linewidth=2)            # Returns a tuple of line objects, thus the comma
 
-    decay = 5
+    decay = 10
     
 #    s2[0, :], rho2[0, :] = getObservation(x_agent2, v_agent2, v_motor2, theta2, 0., w[0, :], z[0, :], (s2[0, ::-1] + z[0, ::-1] / np.sqrt(dt_brain)), 0)
     for i in range(1, iterations - 1):
-        s2[i, :], rho2[i, :] = getObservation(x_agent2, v_agent2, v_motor2, theta2, 0., w[i, :], z[i, :], s2[i - lag, ::-1], z[i - lag, ::-1], i)
+        s2[i, :], rho2[i, :] = getObservation(x_agent2, v_agent2, v_motor2, theta2, 0., w[i, :], z[i, :], s2[i - lag, :], z[i - lag, :], filtered_rho2[i-1, :], i)
 #        s2[i, :], rho2[i, :] = getObservation(x_agent2, v_agent2, v_motor2, theta2, 0., w[i, :], z[i, :], filtered_rho2[i - 1, :], i)
 #        s2[i, :], rho2[i, :] = getObservation(x_agent2, v_agent2, v_motor2, theta2, 0., w[i, :], z[i, :], rho2[i - 1, :], i)
         
-        filtered_rho2[i, :] = filtered_rho2[i - 1, :] + dt_brain * decay * (s2[i, :] + z[i, :] / np.sqrt(dt_brain) - filtered_rho2[i - 1, :])
+        filtered_rho2[i, :] = filtered_rho2[i - 1, :] + dt_brain * decay * (rho2[i, :] - filtered_rho2[i - 1, :])
 #        orientation_endpoint = x_agent2[i, :, None] + length_dir * (np.array([[np.cos(theta2[i])], [np.sin(theta2[i])]]))
 #        orientation = np.concatenate((x_agent2[i, :, None], orientation_endpoint), axis=1)
 #        line1.set_xdata(x_agent2[i, 0])
@@ -157,61 +153,12 @@ def Braitenberg(noise_level, desired_confidence, z2):
 #        line2.set_ydata(orientation[1,:])
 #        fig.canvas.draw()
 #        plt.pause(0.05)
-    
-#    plt.figure()
-#    plt.plot(x_agent2[:, 0], x_agent2[:, 1])
-#    plt.xlim((0,80))
-#    plt.ylim((0,80))
-#    plt.plot(x_light[0], x_light[1], color='orange', marker='o', markersize=20)
-#    plt.plot(x_agent2[0, 0], x_agent2[0, 1], color='red', marker='o', markersize=8)
-#    plt.title('Trajectory (Braitenberg)')
-    
-#    plt.figure()
-#    plt.subplot(1, 2, 1)
-#    plt.plot(x_agent2[:-1, 0], 'b', np.ones(iterations - 1) * x_light[0], 'r')
-#    plt.subplot(1, 2, 2)
-#    plt.plot(x_agent2[:-1, 1], 'b', np.ones(iterations - 1) * x_light[1], 'r')
-#    plt.title('Position')
-#        
-#    plt.figure()
-#    plt.subplot(1, 2, 1)
-#    plt.plot(rho2[:-1, 0], 'b', label='Noisy signal')
-#    plt.plot(s2[:-1, 0], 'g', label='Signal')
-#    plt.legend()
-#    plt.subplot(1, 2, 2)
-#    plt.plot(rho2[:-1, 1], 'b', label='Noisy signal')
-#    plt.plot(s2[:-1, 1], 'g', label='Signal')
-#    plt.legend()
-#    
-#    plt.figure()
-#    plt.plot(v_motor2[:-1, 0], 'b', label='Motor1')
-#    plt.plot(v_motor2[:-1, 1], 'r', label='Motor2')
-#    plt.title('Velocity')
-#    plt.legend()
-    #
-    #plt.figure()
-    #plt.plot(theta2)
-    #plt.title('Orientation')
-    #
-#    points = 100
-#    x_map = range(points)
-#    y_map = range(points)
-#    light = np.zeros((points, points))
-#    
-#    for i in range(points):
-#        for j in range(points):
-#            light[i, j] = light_level(np.array([x_map[j], y_map[i]])) + sigma_z[0] * np.random.randn()
-#    
-#    light_fig = plt.fianagure()
-#    light_map = plt.imshow(light, extent=(0., points, 0., points),
-#               interpolation='nearest', cmap='jet')
-#    cbar = light_fig.colorbar(light_map, shrink=0.5, aspect=5)
-#    
+
     return x_agent2, s2, rho2, filtered_rho2
 
 
 
-noise_level = -5.
+noise_level = -4.
 gamma_z = noise_level * np.ones((obs_states, ))    # log-precisions
 pi_z = np.exp(gamma_z) * np.ones((obs_states, ))
 real_pi_z = np.exp(gamma_z) * np.ones((obs_states, ))
@@ -221,21 +168,21 @@ z = (np.dot(np.diag(sigma_z), np.random.randn(obs_states, iterations))).transpos
 
 agent_position, s, rho, filtered_rho = Braitenberg(noise_level, 0, z)
 
-x_light = np.array([59.,47.])
+#x_light = np.array([59.,47.])
 
 
 plt.figure(figsize=(5, 4))
 plt.plot(agent_position[:, 0], agent_position[:, 1])
 #plt.xlim((0,80))
 #plt.ylim((0,80))
-plt.plot(x_light[0], x_light[1], color='orange', marker='o', markersize=20)
+#plt.plot(x_light[0], x_light[1], color='orange', marker='o', markersize=20)
 plt.plot(agent_position[0, 0], agent_position[0, 1], color='red', marker='o', markersize=8)
 plt.title('Trajectory', fontsize=14)
 
 plt.figure(figsize=(5, 4))
 plt.plot(np.arange(0, T-dt_brain, dt_brain), rho[:-1, 0], 'b', label='Sensory reading $ρ_{l_1}$')
 plt.plot(np.arange(0, T-dt_brain, dt_brain), s[:-1, 0], 'g', label='Sensory reading $ρ_{l_1}$, no noise')
-#plt.plot(np.arange(0, T-dt_brain, dt_brain), filtered_rho[:-1, 0], ':r', label='Belief about sensory reading $\mu_{l_1}$')
+plt.plot(np.arange(0, T-dt_brain, dt_brain), filtered_rho[:-1, 0], ':r', label='Belief about sensory reading $\mu_{l_1}$')
 plt.xlabel('Time (s)')
 plt.ylabel('Luminance')
 #plt.title('Exteroceptor $ρ_{l_1}$, $\mu_{l_1}$', fontsize=14)
@@ -257,4 +204,19 @@ plt.legend(loc = 4)
 #plt.title('Proprioceptor $ρ_{m_2}$, $\mu_{m_2}$', fontsize=14)
 #plt.legend(loc = 4)
 
+
+#points = 100
+#x_map = range(points)
+#y_map = range(points)
+#light = np.zeros((points, points))
+#
+#for i in range(points):
+#    for j in range(points):
+#        aa = light_level(np.array([x_map[j], y_map[i]])) + sigma_z[0] * np.random.randn()
+#        light[i, j] = light_level(np.array([x_map[j], y_map[i]])) + sigma_z[0] * np.random.randn()
+#
+#light_fig = plt.figure()
+#light_map = plt.imshow(light, extent=(0., points, 0., points),
+#           interpolation='nearest', cmap='jet')
+#cbar = light_fig.colorbar(light_map, shrink=0.5, aspect=5)
 
